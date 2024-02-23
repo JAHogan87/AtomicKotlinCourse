2cc7877040d24228528b4c0bc1f856c24e640953
Hogan <JAHogan87@gmail.com>
2024-02-23 10:55:26 -0600

    Update While Exercise 2

--- Programming Basics/Repetition with while/Exercise 2/src/Task.kt
+++ Programming Basics/Repetition with while/Exercise 2/src/Task.kt
@@ -1,10 +1,22 @@
 // RepetitionWithWhile/Task2.kt
 package repetitionWithWhileExercise2
 
-fun sum(number: Int): Int {
-  TODO()
+fun sum(number: Int): Int
+{
+  var count = 0
+  var sum = 0
+
+  while(count<number+1)
+  {
+
+     sum += count
+    count++
+  }
+  return sum
+
 }
 
-fun main() {
+fun main()
+{
   println(sum(10))  // 55
 }
